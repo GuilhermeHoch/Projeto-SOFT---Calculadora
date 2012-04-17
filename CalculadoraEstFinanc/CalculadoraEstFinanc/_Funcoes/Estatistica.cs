73649c2e9f60b2abb823c1876bbdc07045483021
Guilherme Augusto Hoch <guilhermehoch@hotmail.com>
2012-04-17 00:04:39 -0300

    Calculadora V1.0

---
+++ CalculadoraEstFinanc/CalculadoraEstFinanc/_Funcoes/Estatistica.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Estatistica
+{
+    public double calculaMediaAritmetica( double n1
+                                         ,double n2
+                                         ,double n3
+                                         ,double n4
+                                         ,double n5 )
+    {
+        return ((n1 + n2 + n3 + n4 + n5) / 5);
+    }
+
+    public double calculaMediaPonderada( double n1
+                                        ,double n2
+                                        ,double n3
+                                        ,double n4
+                                        ,double n5
+                                        ,double p1
+                                        ,double p2
+                                        ,double p3
+                                        ,double p4
+                                        ,double p5 )
+    {
+         return ( ( (n1*p1) + (n2*p2) + (n3*p3) + (n4*p4) + (n5*p5) ) / ( p1 + p2 + p3 + p4 + p5 ) );
+    }
+
+    public double calculaMediaHarmonica( double n1
+                                        ,double n2
+                                        ,double n3
+                                        ,double n4
+                                        ,double n5 )
+    { 
+         return ( ( (1/n1) + (1/n2) + (1/n3) + (1/n4) + (1/n5) ) / 5 );
+    }
+
+    public double calculaMediaGeometrica( double n1
+                                         ,double n2
+                                         ,double n3
+                                         ,double n4
+                                         ,double n5 )
+    {
+        return (Math.Pow((n1 * n2 * n3 * n4 * n5), 5 ) );
+    }
+
+}
---
+++ CalculadoraEstFinanc/CalculadoraEstFinanc/_Funcoes/Financeira.cs
@@ -0,0 +1,88 @@
+﻿
+using System;
+
+public class Financeira
+{
+    //
+    // --
+    //
+    public double calculaJurosSimples(double capital
+                                     ,double taxa
+                                     ,double tempo)
+    {
+        /* 
+         * Juros Simples
+         * 
+         * J = C * i * t
+         * 
+         * J = juros
+         * C = capital
+         * i = taxa de juros
+         * t = tempo de aplicação (mês, bimestre, trimestre, semestre, ano...)
+        */
+
+        return (capital * taxa * tempo);
+    }
+
+    public double calculaJurosCompostos(double capital
+                                       ,double taxa
+                                       ,double tempo)
+    {
+        /*
+         * M = C * (1 + i)t
+         * M: montante
+         * C: capital
+         * i: taxa de juros
+         * t: tempo de aplicação 
+         */
+
+        return (capital * Math.Pow((1 + taxa), tempo));
+    }
+
+    public double calculaDescontoRacionalSimples(double capital
+                                                ,double taxa
+                                                ,double tempo)
+    {
+        /*
+         * Desconto Simples Racional (por dentro): 
+         *     O cálculo deste desconto funciona análogo ao cálculo dos juros simples, 
+         * substituindo-se o Capital P na fórmula de juros simples pelo Valor Atual A do título.
+         *     O cálculo do desconto racional é feito sobre o Valor Atual do título.
+         */
+
+        return (capital / (1 + (taxa * tempo))); 
+    }
+
+    public double calculaDescontoComercialSimples(double capital
+                                                 ,double taxa
+                                                 ,double tempo)
+    {
+        /*
+         * Desconto Simples Comercial (por fora): 
+         *     O cálculo deste desconto é análogo ao cálculo dos juros simples, substituindo-se 
+         * o Capital P na fórmula de juros simples pelo Valor Nominal N do título.
+         */
+        return (capital * taxa * tempo);
+    }
+
+    public double calculaTaxaNominalJuros(double taxa
+                                         ,double tempo)
+    {
+        /*
+         * in = i * n
+         */
+
+        return ((taxa * tempo) * 100);
+    }
+
+    public double calculaTaxaEfetivaJuros(double taxa
+                                         ,double tempo)
+    {
+        /*
+         * ie = ( 1 + i )n - 1
+         */
+
+        return ((Math.Pow((1 + taxa), tempo)) * 100);
+    } 
+   
+}
